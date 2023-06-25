65d3e89254e797940bef701b42152679e738ede5
yusronnugroho <yusronx2smaboy@gmail.com>
2023-06-25 11:21:41 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Yusron Nugroho Aji" // TODO: replace this
+const studentId = "FE4666470" // TODO: replace this
+
+export { url, studentName, studentId }
